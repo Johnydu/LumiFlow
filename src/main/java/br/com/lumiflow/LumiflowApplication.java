e47aaa1fb02771581b91cf3766546bbf4771e711
Johny Duarte <johny1893@gmail.com>
2026-06-25 23:06:29 -0300

    chore: configurações iniciais do projeto

---
+++ src/main/java/br/com/lumiflow/LumiflowApplication.java
@@ -0,0 +1,13 @@
+package br.com.lumiflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LumiflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LumiflowApplication.class, args);
+	}
+
+}
