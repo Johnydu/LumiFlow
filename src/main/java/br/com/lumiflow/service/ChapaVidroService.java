e4abac46bcf39f417e9a2f562882d07ca64b38e4
Johny Duarte <johny1893@gmail.com>
2026-08-20 00:33:40 -0300

    fix(flyway): remove migrations e arquivos obsoletos

--- src/main/java/br/com/lumiflow/service/ChapaVidroService.java
+++
@@ -1,9 +0,0 @@
-package br.com.lumiflow.service;
-
-import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@AllArgsConstructor
-public class ChapaVidroService {
-}
--- src/main/resources/db/migration/V3__alteracao_tabela_tipo_vidro.sql
+++
@@ -1,5 +0,0 @@
-alter table tipo_vidro
-    rename column codigo_produto to tipo_vidro;
-
-alter table tipo_vidro
-    rename to chapa_vidro;
